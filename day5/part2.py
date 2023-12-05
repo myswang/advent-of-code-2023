996518c91dc13ae2783f9486e0e6b5e1a22737ec
Mike Wang <mike.wang7@gmail.com>
2023-12-05 14:54:26 -0800

    Add more comments

--- day5/part2.py
+++ day5/part2.py
@@ -1,10 +1,13 @@
+# applies a map to a list of seed ranges
 def apply_map(seed_ranges, alm_map):
     res = set()
     while len(seed_ranges) > 0:
         sr = seed_ranges.pop()
         for rag, dif in alm_map.items():
+            # the seed ranges do not overlap
             if sr[0] > rag[1] or sr[1] < rag[0]:
                 continue
+            # overlap detected!!! now check for overhangs
             if sr[1] > rag[1]:
                 seed_ranges.append((rag[1]+1, sr[1]))
                 sr = (sr[0], rag[1])
@@ -17,14 +20,17 @@ def apply_map(seed_ranges, alm_map):
         res.add(sr)
     return(list(res))
 
+# read puzzle input
 with open("input.txt", "r") as input_file:
     blocks = input_file.read().split("\n\n")
 
+# obtain the seed ranges
 seeds = [int(b) for b in blocks[0].split()[1:]]
 seed_ranges = []
 for i in range(0,len(seeds),2):
     seed_ranges.append((seeds[i], seeds[i]+seeds[i+1]-1))
 
+# parse the almanac maps
 almanac = []
 for block in blocks[1:]:
     temp = block.splitlines()[1:]
@@ -34,7 +40,9 @@ for block in blocks[1:]:
         alm_map[(nums[1], nums[1]+nums[2]-1)] = nums[0] - nums[1]
     almanac.append(alm_map)
 
+# apply each map sequentially to the seed ranges
 for alm_map in almanac:
     seed_ranges = apply_map(seed_ranges, alm_map)
 
+# return the minimum low value of a seed range
 print(min([sr[0] for sr in seed_ranges]))
