7ea7827c347a9a840022893232d6fd81ca13b62d
Mike Wang <mike.wang7@gmail.com>
2023-12-08 00:44:59 -0800

    Day 8 part 1 solution

---
+++ day8/part1.py
@@ -0,0 +1,27 @@
+import re
+
+with open("input.txt", "r") as input_file:
+    lines = input_file.read().splitlines()
+
+instructions = [int(step) for step in lines[0].replace("R", " 1").replace("L", " 0").split()]
+
+nodes = {}
+for line in lines[2:]:
+    matches = re.findall(r"\w\w\w", line)
+    nodes[matches[0]] = tuple(matches[1:])
+
+cur_node = "AAA"
+num_steps = 0
+done = False
+while(True):
+    for idx, instruction in enumerate(instructions, 1):
+        if cur_node == "ZZZ":
+            done = True
+            break
+        cur_node = nodes[cur_node][instruction]
+    num_steps += idx
+    if(done):
+        num_steps -= 1
+        break
+
+print(num_steps)
